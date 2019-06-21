e796ffcc2d1b9c86c09045632af86aae52ce213a
Balintataw J <balintataw2@gmail.com>
2019-06-21 14:59:42 -0700

    Update dependencies with security issues, add missing Dockerfile to client, update docker-compose to version 3, created exponential backoff/retry for connecting to mongo

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            apiResponse: "",
-            dbResponse: ""
+            apiResponse: '',
+            dbResponse: '',
         };
     }
 
     // Go to API and check testAPI route for a response
     callAPI() {
-        fetch("http://localhost:9000/testAPI")
+        fetch('http://localhost:9000/testAPI')
             .then(res => res.text())
             .then(res => this.setState({ apiResponse: res }))
             .catch(err => err);
@@ -21,7 +21,7 @@ class App extends Component {
 
     // Go to API and check testDB route for a response
     callDB() {
-        fetch("http://localhost:9000/testDB")
+        fetch('http://localhost:9000/testDB')
             .then(res => res.text())
             .then(res => this.setState({ dbResponse: res }))
             .catch(err => err);
@@ -35,13 +35,13 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <h1 className='App-title'>Welcome to React</h1>
                 </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-                <p className="App-intro">{this.state.dbResponse}</p>
+                <p className='App-intro'>{this.state.apiResponse}</p>
+                <p className='App-intro'>{this.state.dbResponse}</p>
             </div>
         );
     }
